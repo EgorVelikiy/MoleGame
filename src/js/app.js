a85f02415f73b4277500a5f5a18a70b150252419
Egor <egor.velikiy.02@mail.ru>
2024-09-15 17:25:07 +0300

    Updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,47 @@
+class holeWidget {
+  constructor(element) {
+    this._element = element;
+    this.currInd = -1;
+    this.prevInd = -1;
+  }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const widget = new holeWidget(document.querySelector('.hole-game'));
-  window.widget = widget;
-  widget.startGame()
-})
+  getHole(index) {
+    const holesArray = this._element.querySelectorAll(".hole");
+    return holesArray[index];
+  }
+
+  addMole(index) {
+    const mole = document.createElement("img");
+
+    mole.src =
+      "https://github.com/netology-code/ahj-homeworks/raw/video/dom/pic/goblin.png";
+    mole.classList.add("mole");
+
+    const hole = this.getHole(index);
+    console.log(index);
+    hole.appendChild(mole);
+    console.log("from add");
+  }
+
+  removeMole() {
+    document.querySelector(".mole").remove();
+  }
+
+  nextHole() {
+    setInterval(() => {
+      do {
+        this.currInd = Math.floor(Math.random() * 16);
+      } while (this.currInd === this.prevInd);
+
+      if (this.prevInd >= 0) {
+        this.removeMole(this.prevInd);
+      }
+      
+      this.addMole(this.currInd);
+      this.prevInd = this.currInd;
+    }, 1000);
+  }
+}
+
+const start = new holeWidget(document.querySelector('.hole-game'))
+start.nextHole()
